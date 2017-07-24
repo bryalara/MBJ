c47490d3f3133f592e85d9aa6eb380082afe2eb5
Jada <jadalee@college.harvard.edu>
2017-07-24 09:07:03 -0700

    added import at the top of python, doctype for html

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+import webapp2
+import logging
+import urllib #allows you to make communcation to the outside world (to call the https)
+import urllib2
+import jinja2
+
+env=jinja2.Environment(
+    loader=jinja2.FileSystemLoader(
+        os.path.dirname(__file__)))
+
 class MainPage(webapp2.RequestHandler):
     def get(self):
 
